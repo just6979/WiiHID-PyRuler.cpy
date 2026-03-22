85a830d62c72806fef50e4214190d7efe1f24ea9
Justin White <jw@justinwhite.net>
2026-03-22 17:03:49 -0400

    completely remove windows & mac options to save even more memory

--- code.py
+++ code.py
@@ -9,13 +9,7 @@ from adafruit_hid.mouse import Mouse
 from digitalio import DigitalInOut, Direction
 from wiichuck.nunchuk import Nunchuk
 
-# WINDOWS = "W"
-# MAC = "M"
-# LINUX = "L"  # and Chrome OS
-#
-# # Set your computer type to one of the above
-# OS = LINUX
-
+# comment out to save 16 bytes
 print(board.board_id)
 
 kbd = Keyboard(usb_hid.devices)
@@ -64,21 +58,6 @@ def read_caps():
     cap_touches[3] = touches[3].raw_value > 3000
     return cap_touches
 
-
-# def type_alt_code(code):
-#     kbd.press(0xE2)  # KEYCODE_ALT
-#     for c in str(code):
-#         if c == '0':
-#             keycode = 0x62  # KEYPAD_ZERO
-#         elif '1' <= c <= '9':
-#             keycode = 0x59 + ord(c) - ord('1')  # KEYPAD_ONE
-#         else:
-#             continue
-#         kbd.press(keycode)
-#         kbd.release(keycode)
-#     kbd.release_all()
-
-
 key_repeat_delay = .25
 key_last_repeat = 0
 
@@ -111,61 +90,46 @@ while True:
         for i, c in enumerate(caps):
             leds[i].value = c
         if caps[0]:
-            # if OS == WINDOWS:
-            #     type_alt_code(234)
-            # elif OS == MAC:
-            #     kbd.send(0xE2, 0x1D)  # ALT Z
-            # elif OS == LINUX:
-                kbd.press(0xE0, 0xE1)  # CTRL SHIFT
-                kbd.press(0x18)  # U
-                kbd.release_all()
-                kbd.send(0x1F)  # TWO
-                kbd.send(0x1E)  # ONE
-                kbd.send(0x1F)  # TWO
-                kbd.send(0x23)  # SIX
-                kbd.send(0x28)  # ENTER
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x1F)  # TWO
+            kbd.send(0x1E)  # ONE
+            kbd.send(0x1F)  # TWO
+            kbd.send(0x23)  # SIX
+            kbd.send(0x28)  # ENTER
         if caps[1]:
-            # if OS == WINDOWS:
-            #     type_alt_code(230)
-            # elif OS == MAC:
-            #     kbd.send(0xE2, 0x10)  # ALT, M
-            # elif OS == LINUX:
-                kbd.press(0xE0, 0xE1)  # CTRL SHIFT
-                kbd.press(0x18)  # U
-                kbd.release_all()
-                kbd.send(0x27)  # 0
-                kbd.send(0x20)  # 3
-                kbd.send(0x05)  # B
-                kbd.send(0x06)  # C
-                kbd.send(0x28)  # ENTER
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x05)  # B
+            kbd.send(0x06)  # C
+            kbd.send(0x28)  # ENTER
         if caps[2]:
-            # if OS == WINDOWS:
-            #     type_alt_code(227)
-            # elif OS == MAC:
-            #     kbd.send(0xE2, 0x13)  # ALT P
-            # elif OS == LINUX:
-                kbd.press(0xE0, 0xE1)  # CRTL SHIFT
-                kbd.press(0x18)  # U
-                kbd.release_all()
-                kbd.send(0x27)  # 0
-                kbd.send(0x20)  # 3
-                kbd.send(0x06)  # C
-                kbd.send(0x27)  # 0
-                kbd.send(0x28)  # ENTER
+            kbd.press(0xE0, 0xE1)  # CRTL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x06)  # C
+            kbd.send(0x27)  # 0
+            kbd.send(0x28)  # ENTER
         if caps[3]:
             # if OS == WINDOWS:
             #     type_alt_code(231)
             # elif OS == MAC:
             #     pass
             # elif OS == LINUX:
-                kbd.press(0xE0, 0xE1)  # CTRL SHIFT
-                kbd.press(0x18)  # U
-                kbd.release_all()
-                kbd.send(0x27)  # 0
-                kbd.send(0x20)  # 3
-                kbd.send(0x06)  # C
-                kbd.send(0x21)  # 4
-                kbd.send(0x28)  # ENTER
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x06)  # C
+            kbd.send(0x21)  # 4
+            kbd.send(0x28)  # ENTER
 
     if nc:
         try:

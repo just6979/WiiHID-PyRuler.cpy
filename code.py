5f2a256301986f9ce1484796cc837567b4f65eaf
Justin White <jw@justinwhite.net>
2026-03-22 13:08:40 -0400

    inline the keycodes to save memory

--- code.py
+++ code.py
@@ -1,11 +1,12 @@
+import gc
 import os
-import board
-from digitalio import DigitalInOut, Direction
 import time
+import board
 import touchio
-
-# Set this to True to turn the touchpads into a keyboard
-ENABLE_KEYBOARD = True
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.mouse import Mouse
+from digitalio import DigitalInOut, Direction
 
 WINDOWS = "W"
 MAC = "M"
@@ -14,19 +15,19 @@ LINUX = "L"  # and Chrome OS
 # Set your computer type to one of the above
 OS = LINUX
 
-# Used if we do HID output, see below
-if ENABLE_KEYBOARD:
-    import usb_hid
-    from adafruit_hid.keyboard import Keyboard
-    from adafruit_hid.keycode import Keycode
-    from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-    kbd = Keyboard(usb_hid.devices)
-    layout = KeyboardLayoutUS(kbd)
+print(dir(board), os.uname()) # Print a little about ourselves
 
-#print(dir(board), os.uname()) # Print a little about ourselves
+kbd = Keyboard(usb_hid.devices)
+mouse = Mouse(usb_hid.devices)
+sensitivity = 33
+do_clicks = True
+left_down = False
+right_down = False
 
-led = DigitalInOut(board.D13)
-led.direction = Direction.OUTPUT
+
+
+status_led = DigitalInOut(board.LED)
+status_led.direction = Direction.OUTPUT
 
 touches = [DigitalInOut(board.CAP0)]
 for p in (board.CAP1, board.CAP2, board.CAP3):
@@ -37,13 +38,14 @@ for p in (board.LED4, board.LED5, board.LED6, board.LED7):
     led = DigitalInOut(p)
     led.direction = Direction.OUTPUT
     led.value = True
-    time.sleep(0.25)
+    time.sleep(0.1)
     leds.append(led)
-for led in leds:
+for led in reversed(leds):
     led.value = False
-
+    time.sleep(0.1)
 
 cap_touches = [False, False, False, False]
+
 
 def read_caps():
     t0_count = 0
@@ -62,83 +64,80 @@ def read_caps():
     cap_touches[3] = touches[3].raw_value > 3000
     return cap_touches
 
+
 def type_alt_code(code):
-    kbd.press(Keycode.ALT)
+    kbd.press(0xE2)  # KEYCODE_ALT
     for c in str(code):
         if c == '0':
-            keycode = Keycode.KEYPAD_ZERO
+            keycode = 0x62  # KEYPAD_ZERO
         elif '1' <= c <= '9':
-            keycode = Keycode.KEYPAD_ONE + ord(c) - ord('1')
+            keycode = 0x59 + ord(c) - ord('1')  # KEYPAD_ONE
         else:
-            raise RuntimeError("Only number codes permitted!")
+            continue
         kbd.press(keycode)
         kbd.release(keycode)
     kbd.release_all()
 
 while True:
     caps = read_caps()
-    print(caps)
+    # print(caps)
     # light up the matching LED
-    for i,c in enumerate(caps):
+    for i, c in enumerate(caps):
         leds[i].value = c
     if caps[0]:
-        if ENABLE_KEYBOARD:
-            if OS == WINDOWS:
-                type_alt_code(234)
-            elif OS == MAC:
-                kbd.send(Keycode.ALT, Keycode.Z)
-            elif OS == LINUX:
-                kbd.press(Keycode.CONTROL, Keycode.SHIFT)
-                kbd.press(Keycode.U)
-                kbd.release_all()
-                kbd.send(Keycode.TWO)
-                kbd.send(Keycode.ONE)
-                kbd.send(Keycode.TWO)
-                kbd.send(Keycode.SIX)
-                kbd.send(Keycode.ENTER)
+        if OS == WINDOWS:
+            type_alt_code(234)
+        elif OS == MAC:
+            kbd.send(0xE2, 0x1D)  # ALT Z
+        elif OS == LINUX:
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x1F)  # TWO
+            kbd.send(0x1E)  # ONE
+            kbd.send(0x1F)  # TWO
+            kbd.send(0x23)  # SIX
+            kbd.send(0x28)  # ENTER
     if caps[1]:
-        if ENABLE_KEYBOARD:
-            if OS == WINDOWS:
-                type_alt_code(230)
-            elif OS == MAC:
-                kbd.send(Keycode.ALT, Keycode.M)
-            elif OS == LINUX:
-                kbd.press(Keycode.CONTROL, Keycode.SHIFT)
-                kbd.press(Keycode.U)
-                kbd.release_all()
-                kbd.send(Keycode.ZERO)
-                kbd.send(Keycode.THREE)
-                kbd.send(Keycode.B)
-                kbd.send(Keycode.C)
-                kbd.send(Keycode.ENTER)
+        if OS == WINDOWS:
+            type_alt_code(230)
+        elif OS == MAC:
+            kbd.send(0xE2, 0x10)  # ALT, M
+        elif OS == LINUX:
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x05)  # B
+            kbd.send(0x06)  # C
+            kbd.send(0x28)  # ENTER
     if caps[2]:
-        if ENABLE_KEYBOARD:
-            if OS == WINDOWS:
-                type_alt_code(227)
-            elif OS == MAC:
-                kbd.send(Keycode.ALT, Keycode.P)
-            elif OS == LINUX:
-                kbd.press(Keycode.CONTROL, Keycode.SHIFT)
-                kbd.press(Keycode.U)
-                kbd.release_all()
-                kbd.send(Keycode.ZERO)
-                kbd.send(Keycode.THREE)
-                kbd.send(Keycode.C)
-                kbd.send(Keycode.ZERO)
-                kbd.send(Keycode.ENTER)
+        if OS == WINDOWS:
+            type_alt_code(227)
+        elif OS == MAC:
+            kbd.send(0xE2, 0x13)  # ALT P
+        elif OS == LINUX:
+            kbd.press(0xE0, 0xE1)  # CRTL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x06)  # C
+            kbd.send(0x27)  # 0
+            kbd.send(0x28)  # ENTER
     if caps[3]:
-        if ENABLE_KEYBOARD:
-            if OS == WINDOWS:
-                layout.write('https://www.digikey.com/python\n')
-            elif OS == MAC:
-                layout.write('https://www.digikey.com/python\n')
-            elif OS == LINUX:
-                kbd.press(Keycode.CONTROL, Keycode.SHIFT)
-                kbd.press(Keycode.U)
-                kbd.release_all()
-                kbd.send(Keycode.ZERO)
-                kbd.send(Keycode.THREE)
-                kbd.send(Keycode.C)
-                kbd.send(Keycode.FOUR)
-                kbd.send(Keycode.ENTER)
+        if OS == WINDOWS:
+            type_alt_code(231)
+        elif OS == MAC:
+            pass
+        elif OS == LINUX:
+            kbd.press(0xE0, 0xE1)  # CTRL SHIFT
+            kbd.press(0x18)  # U
+            kbd.release_all()
+            kbd.send(0x27)  # 0
+            kbd.send(0x20)  # 3
+            kbd.send(0x06)  # C
+            kbd.send(0x21)  # 4
+            kbd.send(0x28)  # ENTER
     time.sleep(0.1)
